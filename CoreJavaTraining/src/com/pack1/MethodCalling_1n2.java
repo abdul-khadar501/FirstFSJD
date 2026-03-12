48fa7b134318e21f1f2b88e8a71e568324a83dbd
abdul-khadar501 <75967437+abdul-khadar501@users.noreply.github.com>
2026-03-12 15:24:34 +0530

    Add MethodCalling_1n2 class with two methods

--- CoreJavaTraining/src/com/pack1/MethodCalling_1n2.java
+++ CoreJavaTraining/src/com/pack1/MethodCalling_1n2.java
@@ -1,1 +1,27 @@
+package com.pack1;
 
+public class MethodCalling_1n2 
+{
+	public void meth1() // main()
+	{
+		System.out.println("Hi from meth1()");
+		System.out.println("100");
+		System.out.println(100);
+	}
+	public void meth2()
+	{
+		System.out.println("Hello from meth2()");
+		System.out.println(200);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Start");
+		MethodCalling_1n2 aobj=new MethodCalling_1n2();
+		// Creating an object
+		aobj.meth1(); // Calling a method 1
+		aobj.meth2(); // Calling a method 2
+		System.out.println("Java is Awesome");
+		System.out.println("End");
+	}
+
+}
